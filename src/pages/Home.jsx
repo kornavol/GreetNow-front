6f71e12cdfca351373591ff459ddcfe204670df6
Pio-js <pio.saija@web.de>
2021-06-28 16:02:48 +0200

    changed just how to create card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,13 @@ export default function Home() {
     return (
         <div>
             <h4>Presentation</h4>
-            <h4>Create cards buttons</h4>
+            <div>
+                <h3>You can decide how to create a card</h3>
+                <p>Choose <b>Auto</b> to create a random card after selecting a category</p>
+                <p>Choose <b>Custom</b> to create a card step by step selecting images and texts</p>
+                <button>Auto</button>
+                <button>Custom</button>
+            </div>
             <h4>Benefits features</h4>
             <h4>Users reviews</h4>
         </div>
