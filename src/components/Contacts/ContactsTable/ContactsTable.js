2fc243260147dcda44a1e616abafac986a1677c3
kornavol <volosh.andrey@gmail.com>
2021-08-10 09:45:24 +0200

    prepared basic structure for a new contact list

---
+++ src/components/Contacts/ContactsTable/ContactsTable.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useMemo } from "react";
+
+export default function ContactsTable({ entities }) {
+    return (
+        <div>
+
+        </div>
+    );
+}
--- src/pages/dashboard/ContactList.jsx
+++ src/pages/dashboard/ContactList.jsx
@@ -1,56 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './css/ContactList.css';
-import ContactsTable from '../../components/Contacts/ContatsTable/ContactsTable';
+import ContactsTable from '../../components/Contacts/ContactsTable/ContactsTable';
 
 export default function ContactList() {
+
+    const [contacts, setContacts] = useState([{
+        _id: "13asdsad1",
+        firstName: "Sonni",
+        lastName: "Gabotti",
+        dateOfBbirth: "10/14/1950",
+        gender: "Female",
+        relationship:['family'],
+        events:['birthday', 'Christmas']
+      },
+      {
+        _id: "13asdsad1",
+        firstName: "Andreas",
+        lastName: "Kornblum",
+        dateOfBbirth: "08/07/1986",
+        gender: "Male",
+        relationship:['family'],
+        events:['birthday', 'Christmas']
+      }]);
     
-    /* data set from Back-end */
-    const entities = [
-        {
-            "_id": "13asdsad1",
-            "id": 1,
-            "firstName": "Sonni",
-            "lastName": "Gabotti",
-            "email": "sgabotti0@wsj.com",
-            "userName": "sgabotti0",
-            "gender": "Female",
-            "status": 0,
-            "dateOfBbirth": "10/14/1950",
-            "ipAddress": "251.237.126.210",
-            "type": 1,
-            "_userId": 1,
-            "_createdDate": "09/07/2016",
-            "_updatedDate": "05/31/2013"
-          },
-          {
-            "_id": "13asdsad2",
-            "id": 2,
-            "firstName": "Adreas",
-            "lastName": "Kornblum",
-            "email": "sgabotti0@wsj.com",
-            "userName": "sgabotti0",
-            "gender": "Female",
-            "status": 0,
-            "dateOfBbirth": "08/07/1986",
-            "ipAddress": "251.237.126.210",
-            "type": 1,
-            "_userId": 1,
-            "_createdDate": "09/07/2016",
-            "_updatedDate": "05/31/2013"
-          },
-    ]
-
-
 
     return (
         <div className="page">
-            {/* Add:
+            Add:
             Filters
             Search
             Button: Add new custommer
 
-            */}
-            <ContactsTable entities = {entities}/>
+           
+            {/* <ContactsTable entities = {entities}/> */}
             {/* Pagination */}
         </div>
     );
