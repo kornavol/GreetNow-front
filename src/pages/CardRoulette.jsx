caa98cbf4dd81f9804efb3d2edaaa9158491db7a
Pio-js <71008079+Pio-js@users.noreply.github.com>
2021-08-23 16:26:34 +0200

    Update CardRoulette.jsx

--- src/pages/CardRoulette.jsx
+++ src/pages/CardRoulette.jsx
@@ -44,9 +44,6 @@ const CardRoulette = (props) => {
                 <div className="balloon2">
                     <img src={balloonWhite} alt="balloon"/>
                 </div>
-                <div id="card-roulette-btn" className="card-roulette-button glow-on-hover">
-                    <Link className="roulette-btn">SPIN</Link>
-                </div>
                 </div>
             </section>
     );
