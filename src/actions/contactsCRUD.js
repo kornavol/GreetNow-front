e2fa56399972f19385dad6cd3f097fea7fb93fcf
Restyled.io <commits@restyled.io>
2021-08-23 15:12:39 +0000

    Restyled by clang-format

--- src/actions/contactsCRUD.js
+++ src/actions/contactsCRUD.js
@@ -5,41 +5,42 @@
 //     Authorization: `Bearer ${localStorage.getItem('authToken')}`
 // }
 
-export const getAllContacts = () => {
-    const url = 'http://localhost:8080/recipients/getAll'
-    const options = {
-        method: 'GET',
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem('authToken')}`
+export const getAllContacts =
+    () => {
+      const url = 'http://localhost:8080/recipients/getAll'
+      const options = {
+        method : 'GET',
+        headers : {
+          "Content-Type" : "application/json",
+          Authorization : `Bearer ${localStorage.getItem('authToken')}`
         }
-    }
+      }
 
-    /* dispatch comes from middleware */
-    return async dispatch => {
+      /* dispatch comes from middleware */
+      return async dispatch => {
         const response = await fetch(url, options);
         const result = await response.json()
         const contacts = result.data
-        dispatch({ type: "GET_ALL_CONTACTS", contacts })
+        dispatch({type : "GET_ALL_CONTACTS", contacts})
+      }
     }
-}
 
 export const deleteContact = (contact) => {
-    const url = 'http://localhost:8080/recipients/delete_record'
-    const options = {
-        method: 'DELETE',
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem('authToken')}`
-        },
-        body: JSON.stringify(contact)
-    }
+  const url = 'http://localhost:8080/recipients/delete_record'
+  const options = {
+    method : 'DELETE',
+    headers : {
+      "Content-Type" : "application/json",
+      Authorization : `Bearer ${localStorage.getItem('authToken')}`
+    },
+    body : JSON.stringify(contact)
+  }
 
-    /* dispatch comes from middleware */
-    return async dispatch => {
-        const response = await fetch(url, options);
-        const result = await response.json()
-        // dispatch({ type: "DELETE_CONTACTS", result })
-        return result
-    }
+  /* dispatch comes from middleware */
+  return async dispatch => {
+    const response = await fetch(url, options);
+    const result = await response.json()
+    // dispatch({ type: "DELETE_CONTACTS", result })
+    return result
+  }
 }
