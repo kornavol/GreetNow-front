83c8197daba9101c28a0ba7860ef64930b888e25
kornavol <volosh.andrey@gmail.com>
2021-08-25 21:26:18 +0200

    fix bug with picture beetwen cardEditor and Previe (editing) and Previe (with url)

--- src/reducer/currPict.js
+++ src/reducer/currPict.js
@@ -1,6 +1,9 @@
 /* To-do. Change logic of initial picture  */
 
+
 const currPict = (state = {name: 'cover-card-editor.png'}, action) => {
+    
+    console.log('from CurrPic Dtorage:', state  );
     switch (action.type) {
         case "SEND_PICT":
             state = action.picture
